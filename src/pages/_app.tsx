2c11a62fc7495e3bf101b72acd2277041c190dc0
Yasin Eryılmaz <yasineryilmazfb@gmail.com>
2023-04-22 00:15:15 +0300

    Installed main packages and files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '<src>/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "<src>/styles/globals.css";
+import type { AppProps } from "next/app";
+import ScopedCssBaseline from "@mui/material/ScopedCssBaseline";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
---
+++ src/styles/index.styles.js
@@ -0,0 +1,13 @@
+import { css } from "@emotion/css";
+
+export default {
+  main: css`
+    color: white;
+    background: transparent;
+
+    & .app-title {
+      font-weight: 300;
+      margin: 25px 0;
+    }
+  `,
+};
