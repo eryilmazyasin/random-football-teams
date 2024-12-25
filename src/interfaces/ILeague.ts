2de2fb7fd1b3b0603760621132196a8877dc6735
Yasin Eryılmaz <40694757+eryilmazyasin@users.noreply.github.com>
2024-12-25 23:06:05 +0300

    90-100 interface added to interface.ts - Update ILeague.ts

--- src/interfaces/ILeague.ts
+++ src/interfaces/ILeague.ts
@@ -7,7 +7,7 @@ export type ILeagues =
   | "Fransa Ligue 1"
   | "Portekiz Primeira Ligue";
 
-export type ITeamPower = "All" | "0 - 50" | "50 - 70" | "70 - 80" | "80 - 100";
+export type ITeamPower = "All" | "0 - 50" | "50 - 70" | "70 - 80" | "80 - 100" | "90 - 100";
 
 export interface ITeams {
   team_ligue: string;
